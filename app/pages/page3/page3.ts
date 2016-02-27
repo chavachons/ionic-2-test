e31875a8b9b602fd68c9cc935d21fec6f9a87d79
Chavachon Supreeyapond <chavachons@gmail.com>
2016-02-27 16:28:39 +0700

    add file

--- app/pages/page3/page3.ts
+++ app/pages/page3/page3.ts
@@ -2,6 +2,11 @@ import {Page} from 'ionic-framework/ionic';
 
 
 @Page({
-  templateUrl: 'build/pages/page3/page3.html'
+    templateUrl: 'build/pages/page3/page3.html'
 })
-export class Page3 {}
+export class Page3 {
+    menuShop:string;
+    constructor() {
+        this.menuShop = "Menu";
+    }
+}
